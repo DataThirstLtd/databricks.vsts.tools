4ecf983f5105175888b6fe20a577a27ebeb4f7c7
Simon D'Morias <simon@Simons-MacBook-Pro.local>
2018-11-14 16:01:33 +0000

    rev version

--- build.ps1
+++ build.ps1
@@ -1,10 +1,9 @@
 
 param([string]$Config = 'Test', [boolean]$Clean = $false)   # Test Or Prod
 
-# These do nothing now, the --rev-version is used when deploying
 $VersionMajor = "0"
-$VersionMinor = "0"
-$VersionPatch = "0"
+$VersionMinor = "3"
+$VersionPatch = "4"
 
 Set-Location $PSScriptRoot
 
